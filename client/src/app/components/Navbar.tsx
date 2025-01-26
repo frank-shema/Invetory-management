f93619b0d4540c8710273ad838d9bdc5725677ba
frank-shema <shemafranko0987@gmail.com>
2025-01-26 15:52:19 +0200

    changed navbar

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-      This is the navbar
-    </div>
+    <nav className="bg-blue-600 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-xl font-bold">MyApp</div>
+        <ul className="flex space-x-4">
+          <li>
+          </li>
+          <li>
+            <a href="/about" className="hover:underline">About</a>
+          </li>
+          <li>
+            <a href="/contact" className="hover:underline">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
