dbcf1a76766a00b8cc573d5ebb69c2a24d922f78
frank-shema <shemafranko0987@gmail.com>
2025-01-27 21:39:08 +0200

    final commit done

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -93,7 +93,7 @@ const Sidebar = () => {
             isSidebarCollapsed ? "hidden" : "block"
           } font-extrabold text-2xl`}
         >
-          EDSTOCK
+          AM STOCK
         </h1>
 
         <button
